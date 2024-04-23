caaa5fb87d13503037a4a81214f690876d65e1a6
Somto Ufondu <somtoufondu@gmail.com>
2024-04-23 04:35:22 -0600

    Add process.exit() in the error check for fetchBreedDescription(). Add unit tests to test the breedFetcher.js code.

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ const filePath = userInput[1];
 fetchBreedDescription(breedName, (error, desc) => {
   if (error) {
     console.log("Error fetch details:", error);
+    process.exit();
   } else {
     console.log(desc);
 
